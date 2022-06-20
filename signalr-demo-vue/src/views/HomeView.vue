ed2b9c7e78b59fc574c0768f7d1c2791bc0d8037
Stefano Previato <frikyfriky11@gmail.com>
2022-06-20 14:06:06 +0200

    Implemented sample client call

--- signalr-demo-vue/src/views/HomeView.vue
+++ signalr-demo-vue/src/views/HomeView.vue
@@ -1,10 +1,36 @@
 <template>
-  <h2>Home view</h2>
+  <div>
+    <h2>Home view</h2>
+
+    <ul>
+      <li v-for="customer in customers" :key="customer.id">
+        {{ customer.id }} {{ customer.name }}
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
+import { HubConnection, HubConnectionBuilder } from "@microsoft/signalr";
 
 @Component
-export default class HomeView extends Vue {}
+export default class HomeView extends Vue {
+  customers: { id: number; name: string }[] = [];
+
+  customersHub?: HubConnection = undefined;
+
+  async created() {
+    this.customersHub = new HubConnectionBuilder()
+      .withUrl("https://localhost:7100/signalr/customers-hub")
+      .withAutomaticReconnect()
+      .build();
+
+    this.customersHub.start();
+
+    this.customersHub.on("CustomerCreated", (id: number, name: string) => {
+      this.customers.push({ id, name });
+    });
+  }
+}
 </script>
