05d3a6aed3012ef0c769ee49f8aed522b39b6fe0
Stefano Previato <frikyfriky11@gmail.com>
2022-06-20 11:49:52 +0200

    Added sample Vue.js project

---
+++ signalr-demo-vue/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>SignalR Demo - Vue.js</h1>
+    <router-view />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class App extends Vue {}
+</script>
---
+++ signalr-demo-vue/src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<template>
+  <h2>Home view</h2>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class HomeView extends Vue {}
+</script>
